db2a5c7b6b821547a9d73c8ee9dcc2e5edb90fe6
user <user@a2ylife.cf>
2018-02-24 18:37:58 -0600

    updates

--- xref.c
+++ xref.c
@@ -19,45 +19,46 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#define MAXWORD 100000
+#define F       0
+#define T       1
+#define N       NULL
+#define Z       0
+#define O       1
+#define MXWORD  100000
 #define BUFSIZE 100000
-#define FALSE   0
-#define TRUE    1
 
-int cLine    = 1;
-int bufferp  = 0;
+int cLine    = O;
+int bufferp  = Z;
 char buffer[BUFSIZE];
 /* Tree Node */
-struct tnode {	
+struct tnode{	
 	char *word;  	
 	int line; 
 	struct tnode *left; 
 	struct tnode *right;
 };
 /* Detects when there's: 'space', '\f', '\r', '\t', '\v' */
-char isspaceL(char c) {
-	if ( c == ' ' || c == '\f' || c == '\r' || c == '\t' || c == '\v'){ 
-		return 1;
-	}else{  return 0; }
+char isspaceL( char i ){
+	if ( i == ' ' || i == '\f' || i == '\r' || i == '\t' || i == '\v'){ return T; } else{ return F; }
 }
 /* Gets next/word */
-int getWord(char *word, int lim) {
+int getWord( char *word, int lim ){
 	int c;
 	int getch(void);
 	void ungetch(int);
 	char *wrd = word;
-	while (isspaceL(c = getch()))
+	while(isspaceL( c = getch()))
         ;
 		if(c != EOF){ *wrd++ = c;}
 		if(c == '\n'){ ++cLine;}
 		if(!isalpha(c)){ *wrd = '\0'; return c; }
-	for(; --lim > 0; wrd++) {
+	for(; --lim > Z; wrd++) {
 		if(!isalnum(*wrd = getch())){ ungetch(*wrd); break; }
 	} *wrd = '\0';
-	return word[0];
+	return word[Z];
 }
 /* Gets data from buffer */
-int getch(void){ return (bufferp > 0) ? buffer[--bufferp] : getchar(); }
+int getch( void ){ return (bufferp > Z) ? buffer[--bufferp] : getchar(); }
 /* Puts data into buffer */
 void ungetch( int c ){
 	if(bufferp >= BUFSIZE){ 
@@ -65,74 +66,72 @@ void ungetch( int c ){
 	}else{	buffer[bufferp++] = c; }
 }
 /* Verifies we have have a datatype */
-int dataType( char *wrd ) {
+int dataType( char *wrd ){
 	char *dTypes[] = { "char", "double", "float", "int", "long", "short", };
-	int loc    = TRUE;
-	int stop   = 0;
-	int low    = 0;
-	int diff   = 0;
-	int high   = sizeof dTypes / sizeof dTypes[0] - 1;
+	int loc    = T;
+	int stop   = Z;
+	int low    = Z;
+	int diff   = Z;
+	int high   = sizeof dTypes / sizeof dTypes[Z] - O;
 	int where  = ( high / 2 );	
-	if(wrd != NULL) {
-		while(loc && !stop) {
+	if(wrd != N){
+		while(loc && !stop){
 			diff = strcmp(dTypes[where], wrd);
-			if( 0 == diff) { loc = FALSE;} 
-			if( 0 < diff ) {
-				high   = where - 1;
-			}else{
-				low    = where + 1;
-			}
-			if( high <= ( low - 1 )) {
-				stop   = 1;
-			}else{
-				where  = (( high + low ) / 2);
-			}
+			if( Z == diff) { loc    = F; } 
+			if( Z < diff ) { high   = where - O;
+			}else{ low = where + O; }
+			if( high <= ( low - O )) { stop   = O;
+			}else{ where  = (( high + low ) / 2); }
 		}
 	}
 	return loc;
 }
 /* Makes sure that there is a valid Identifier */
-int validIdentifier(char *str) {
-    	if(isdigit(str[0])){    return TRUE; }
-	if(!(isdigit(str[0]))){ return FALSE;}
+int validIdentifier( char *str ){
+    	if(isdigit(str[Z])){    return T; }
+	if(!(isdigit(str[Z]))){ return F; }
 }
 /* Manages/Makes memory for tree node */
-struct tnode *talloc(void){ return(struct tnode *)malloc(sizeof(struct tnode)); }
+struct tnode *talloc( void ){ return(struct tnode *)malloc(sizeof(struct tnode)); }
 /* Adds a tree node */   
-struct tnode *addtree(struct tnode *rt, char *wrd) {
+struct tnode *addtree( struct tnode *rt, char *wrd ){
 	int cdt;
-	if( rt == NULL ){
+	if( rt == N ){
 		rt = talloc();                        
 		rt -> word = strdup(wrd);
 		rt -> line = cLine;
-		rt -> left = rt -> right = NULL;
-	}else if(( cdt = strcmp(wrd,  rt -> word )) < 0 ){
+		rt -> left = rt -> right = N;
+	}else if(( cdt = strcmp(wrd,  rt -> word )) < Z ){
 		rt -> left  = addtree(rt -> left, wrd);
 	}else{  rt -> right = addtree(rt -> right, wrd); }
 	return rt;
 }
 /* Prints tree in order */
-void printTree(struct tnode *rt) {
-	if(rt != NULL){
+void printTree( struct tnode *rt ){
+	if(rt != N){
 		printTree( rt -> left  );
 		printf("  %4d     %s\n", rt -> line, rt -> word);
 		printTree( rt -> right );
 	}
 }
-int main(void) {                      
-	struct tnode *root;
-	char word[MAXWORD];
-	root = NULL;	
-	while(getWord(word, MAXWORD) != EOF) {
-		if (dataType(word) == FALSE) {
-			if (getWord(word, MAXWORD) != EOF) {
-				if(isalpha(word[0])){ root = addtree(root, word); }
-			}
-		}
-	}
+/* Prints Data */
+void prtData( struct tnode *rt ){
 	printf("    XREF Utility\n");
 	printf("-- Ascending Order --\n");
 	printf("   Line | Identifier\n");
-	printTree(root);
-	exit(0);
+	printTree(rt);
+}
+int main( void ) {                      
+	struct tnode *root;
+	char word[MXWORD];
+	root = N;	
+	while( getWord(word, MXWORD) != EOF ){
+		if( dataType(word) == F ){
+			if( getWord(word, MXWORD) != EOF ) {
+				if( isalpha(word[Z])){ root = addtree( root, word ); }
+			}
+		}
+	}
+	prtData(root);
+	exit(Z);
 }
